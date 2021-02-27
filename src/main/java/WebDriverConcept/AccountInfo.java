7393376b7989762666a1d9207a0cf70758808110
itspadmaja <itspadmaja@gmail.com>
2021-02-26 22:52:02 -0500

    updated info on accountPage

--- src/main/java/WebDriverConcept/AccountInfo.java
+++ src/main/java/WebDriverConcept/AccountInfo.java
@@ -1,9 +1,16 @@
 package WebDriverConcept;
 
+import org.openqa.selenium.By;
+
 public class AccountInfo {
+	
+	By emailId = By.id("username");
 	
 	public void Account() {
 		System.out.println("Created Account information ");
+	}
+	public void AccPage() {
+		System.out.println("added info to the page");
 	}
 
 }
